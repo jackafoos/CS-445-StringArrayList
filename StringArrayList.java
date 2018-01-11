c66611df0761454e12a318c3b7c8223111b612e4
Jackafoos <jem319@pitt.edu>
2018-01-11 09:29:54 -0500

    Implemented the instructor's interface at basic level to test with JUnit

---
+++ StringArrayList.java
@@ -0,0 +1,32 @@
+//Jacob McAfoos
+public class StringArrayList implements StringList{
+  public int add(String s){
+    return 0;
+  }
+  public String get(int i){
+    return "";
+  }
+  public boolean contains(String s){
+    return true
+  }
+  public int indexOf(String s){
+    return 0;
+  }
+  public int size(){
+    return 0;
+  }
+  public int add(int index, String s){
+    return 0;
+  }
+  public void clear(){}
+  public boolean isEmpty(){
+    return true;
+  }
+  public String remove(int i){
+    return "";
+  }
+  public void set(int index, String s){}
+  public String[] toArray(){
+    return new String[1];
+  }
+}
