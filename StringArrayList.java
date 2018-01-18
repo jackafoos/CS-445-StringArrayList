39fb0b53794b286077e126112b685b80d2c57fa3
Jackafoos <jem319@pitt.edu>
2018-01-17 20:44:54 -0500

    Added constructor and private properties

--- StringArrayList.java
+++ StringArrayList.java
@@ -1,31 +1,50 @@
 //Jacob McAfoos
 public class StringArrayList implements StringList{
+  private int size;
+  private int capacity;
+  private String[] array;
+
+  public StringArrayList(){
+    array = new String[1];
+    size = 0;
+    capacity = array.length;
+  }
+
   public int add(String s){
     return 0;
   }
+
   public String get(int i){
     return "";
   }
+
   public boolean contains(String s){
-    return true
+    return true;
   }
+
   public int indexOf(String s){
     return 0;
   }
+
   public int size(){
     return 0;
   }
+
   public int add(int index, String s){
     return 0;
   }
+
   public void clear(){}
+
   public boolean isEmpty(){
     return true;
   }
+
   public String remove(int i){
     return "";
   }
   public void set(int index, String s){}
+
   public String[] toArray(){
     return new String[1];
   }
